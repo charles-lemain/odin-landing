22a2db1ef01a174777feb74f181ce5a739992c2c
Karol <kstachowicz@icloud.com>
2022-04-27 23:01:09 +0200

    Add some features and new text content, bug fixes.
    Features include:
    
    Annoying popups
    More padding in more places
    Insight into page creator
    A javascript file is now included so more fun

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('keydown', function(event){
+    if (event.code = 'F12') {
+        alert('Winner! Click to download 2000BTC now!');
+        this.alert('Just kidding lol. Check the box below to disable this popup');
+    }
+});
